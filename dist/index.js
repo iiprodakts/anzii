13ec012c07f3beac0f7f33787c80942e13e4c024
Surprise <info@turtrozz.com>
2020-10-01 20:08:16 +0200

    Implement 'domain' module
    Prior to the 'view' module, anzii framework didn't have the mechanism to handle application wide common tasks such as setting a directory for serving static files, with the implementation of the 'domain' module, this functionality has been integrated into the system.

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+module.exports=function(e){var r={};function t(o){if(r[o])return r[o].exports;var l=r[o]={i:o,l:!1,exports:{}};return e[o].call(l.exports,l,l.exports,t),l.l=!0,l.exports}return t.m=e,t.c=r,t.d=function(e,r,o){t.o(e,r)||Object.defineProperty(e,r,{enumerable:!0,get:o})},t.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},t.t=function(e,r){if(1&r&&(e=t(e)),8&r)return e;if(4&r&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(t.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&r&&"string"!=typeof e)for(var l in e)t.d(o,l,function(r){return e[r]}.bind(null,l));return o},t.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(r,"a",r),r},t.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},t.p="",t(t.s=0)}({"./lib/start.js":
+/*!**********************!*\
+  !*** ./lib/start.js ***!
+  \**********************/
+/*! no static exports found */function(module,exports){eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: ENOENT: no such file or directory, open 'D:\\\\Turtrozz\\\\tools\\\\frameworks\\\\anzii\\\\lib\\\\start.js'\");\n\n//# sourceURL=webpack:///./lib/start.js?")},0:
+/*!*****************************************!*\
+  !*** multi @babel/polyfill ./lib/start ***!
+  \*****************************************/
+/*! no static exports found */function(module,exports,__webpack_require__){eval('__webpack_require__(/*! @babel/polyfill */"@babel/polyfill");\nmodule.exports = __webpack_require__(/*! ./lib/start */"./lib/start.js");\n\n\n//# sourceURL=webpack:///multi_@babel/polyfill_./lib/start?')},"@babel/polyfill":
+/*!**********************************!*\
+  !*** external "@babel/polyfill" ***!
+  \**********************************/
+/*! no static exports found */function(module,exports){eval('module.exports = require("@babel/polyfill");\n\n//# sourceURL=webpack:///external_%22@babel/polyfill%22?')}});
