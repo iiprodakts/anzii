6cdbaab344bd2d917383d7cf8e596518faca0e10
ntsakosurprise <mashelesepru@gmail.com>
2024-01-04 14:28:37 +0200

    chore(anzii): formats webpack config

--- packages/anzii/webpack.config.js
+++ packages/anzii/webpack.config.js
@@ -1,96 +1,82 @@
-
-
-import path from 'path'
-import webpack from 'webpack'
+import path from "path";
+import webpack from "webpack";
 //import nodeExternals from 'webpack-node-externals'
 //import FileManagerPlugin from 'filemanager-webpack-plugin';
 //import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 //const TerserPlugin = require('terser-webpack-plugin');
 
-
-// const NodemonPlugin = require('nodemon-webpack-plugin') 
-const root = path.resolve(__dirname)
+// const NodemonPlugin = require('nodemon-webpack-plugin')
+const root = path.resolve(__dirname);
 // const outsideDir = `${path.resolve(root,`..${path.sep}`)}${path.sep}test.jsx`
 // console.log("THE ROOT",outsideDir)
 
 // console.log('THE ROOT IN WEBPACK')
 // console.log(root)
 
-
-
-
-
-
-const anzii = function(){ 
-  
-  console.log("THE PROCESS MODE IS", process.env.NODE_MODE)
-  console.log("THE ENV IS", process.env.NODE_ENV)
-  console.log("OUR ROOT",root)
-  const isESM = process.env.NODE_MODE === 'esm' ? true : false
-  console.log("isESM",isESM)
+const anzii = function () {
+  console.log("THE PROCESS MODE IS", process.env.NODE_MODE);
+  console.log("THE ENV IS", process.env.NODE_ENV);
+  console.log("OUR ROOT", root);
+  const isESM = process.env.NODE_MODE === "esm" ? true : false;
+  console.log("isESM", isESM);
 
   const config = {
+    entry: ["./lib/start"],
+    target: isESM ? false : "node",
+    // mode: 'development',
+    // devtool: 'source-map',
+    experiments: {
+      outputModule: isESM ? isESM : false,
+    },
+    cache: false,
+    // externals: [nodeExternals()],
+    externalsPresets: {
+      node: true, // in order to ignore built-in modules like path, fs, etc.
+    },
 
-  
-  entry: ['./lib/start'],
-  target: isESM ? false : 'node',
-  // mode: 'development',
-  // devtool: 'source-map',
-  experiments:{
-    outputModule: isESM ? isESM : false
-  },
-  cache:false,
-  // externals: [nodeExternals()],
-  externalsPresets: {
-    node: true // in order to ignore built-in modules like path, fs, etc. 
-  },
-  
-  output: {
-    path: path.resolve('dist'),
-    filename: isESM ? "index.mjs" : 'index.cjs',
-    libraryTarget: isESM  ? "module" : 'commonjs2',
-    chunkFormat: isESM  ? "module" : 'commonjs',
-    // clean: true
-  },
-  module: {
-    rules: [
-      { 
-        test: /\.(js|jsx)$/, 
-        include: [root],
-        use: 'babel-loader',
-       
-      }
+    output: {
+      path: path.resolve("dist"),
+      filename: isESM ? "index.mjs" : "index.cjs",
+      libraryTarget: isESM ? "module" : "commonjs2",
+      chunkFormat: isESM ? "module" : "commonjs",
+      // clean: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          include: [root],
+          use: "babel-loader",
+        },
+      ],
+    },
+
+    plugins: [
+      new webpack.DefinePlugin({
+        __isBrowser__: "false",
+      }),
+      // new FileManagerPlugin({
+      //   onEnd: {
+      //       copy: [
+      //           { source:  path.resolve(__dirname, 'dist','index.cjs'), destination: path.resolve(__dirname, 'lib','index.cjs') },
+      //           // { source:  path.resolve(__dirname, 'dist','index.js.map'), destination: path.resolve(__dirname, 'lib','index.js.map') }
+      //           // { source: '/path/**/*.js', destination: '/path' },
+      //           // { source: '/path/fromfile.txt', destination: '/path/tofile.txt' },
+      //           // { source: '/path/**/*.{html,js}', destination: '/path/to' },
+      //           // { source: '/path/{file1,file2}.js', destination: '/path/to' },
+      //           // { source: '/path/file-[hash].js', destination: '/path/to' }
+      //       ]
+      //     }
+      //   })
     ],
-   
-    
-  },
-
-  plugins: [
-    new webpack.DefinePlugin({
-      __isBrowser__: "false"
-    }),
-    // new FileManagerPlugin({
-    //   onEnd: {
-    //       copy: [
-    //           { source:  path.resolve(__dirname, 'dist','index.cjs'), destination: path.resolve(__dirname, 'lib','index.cjs') },
-    //           // { source:  path.resolve(__dirname, 'dist','index.js.map'), destination: path.resolve(__dirname, 'lib','index.js.map') }
-    //           // { source: '/path/**/*.js', destination: '/path' },
-    //           // { source: '/path/fromfile.txt', destination: '/path/tofile.txt' },
-    //           // { source: '/path/**/*.{html,js}', destination: '/path/to' },
-    //           // { source: '/path/{file1,file2}.js', destination: '/path/to' },
-    //           // { source: '/path/file-[hash].js', destination: '/path/to' }
-    //       ]
-    //     }
-    //   })
-  ],
-  resolve: {
-    roots: [root]
-  },
-  // optimization: {
-  //   minimizer:[]
+    resolve: {
+      roots: [root],
+    },
+    // optimization: {
+    //   minimizer:[]
     // minimizer: [new UglifyJsPlugin({
     //   uglifyOptions:{
     //     keep_classnames: false,
@@ -98,15 +84,12 @@ const anzii = function(){
     //     mangle: true
     //   }
     // })],
-  //},
-  // devtool: "source-map"
-
- 
-            }
-console.log("THE CONFIG", config)
-return config
-}
-
+    //},
+    // devtool: "source-map"
+  };
+  console.log("THE CONFIG", config);
+  return config;
+};
 
 // var le = {
 
@@ -130,10 +113,10 @@ return config
 //   },
 //   module: {
 //     rules: [
-//       { 
-//         test: /\.(js)$/, 
+//       {
+//         test: /\.(js)$/,
 //         use: 'babel-loader',
-       
+
 //       }
 //     ]
 //   },
@@ -143,7 +126,6 @@ return config
 //     })
 //   ]
 // }
-
 
 // var le2 = {
 
@@ -167,10 +149,10 @@ return config
 //   },
 //   module: {
 //     rules: [
-//       { 
-//         test: /\.(js)$/, 
+//       {
+//         test: /\.(js)$/,
 //         use: 'babel-loader',
-       
+
 //       }
 //     ]
 //   },
@@ -181,13 +163,7 @@ return config
 //   ]
 // }
 
-
-
-
-
-
-
-export default anzii
+export default anzii;
 
 // import path from "path";
 // import * as webpacks from "webpack";
@@ -202,7 +178,7 @@ export default anzii
 // console.log("Webpack dir name", __dirname);
 // console.log("Webpack",webpack)
 // // import TerserPlugin from "terser-webpack-plugin";
-// // const NodemonPlugin = require('nodemon-webpack-plugin') 
+// // const NodemonPlugin = require('nodemon-webpack-plugin')
 // const root = path.resolve(__dirname);
 // // console.log('THE ROOT IN WEBPACK')
 // // console.log(root)
@@ -231,9 +207,9 @@ export default anzii
 //                 test: /\.(js|jsx)$/,
 //                 exclude: /(node_module)/,
 //                 use: 'babel-loader',
-               
+
 //               },
-           
+
 //         ],
 //     },
 //     plugins: [
@@ -270,6 +246,3 @@ export default anzii
 //     // devtool: "source-map"
 // };
 // export default [anzii];
-
-
-
