e8f9cfe43ed8859e45e35558c6434a120bd7b090
ntsakosurprise <mashelesepru@gmail.com>
2024-01-04 09:36:55 +0200

    chore(root): create anzii test package

---
+++ packages/anzii-mjs-demo/__tests__/anzii-mjs-demo.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const anziiMjsDemo = require('..');
+
+describe('anzii-mjs-demo', () => {
+    it('needs tests');
+});
---
+++ packages/anzii-mjs-demo/lib/anzii-mjs-demo.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = anziiMjsDemo;
+
+function anziiMjsDemo() {
+    // TODO
+}
