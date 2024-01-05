e0ece918b92e3c73809bb4c0d8857d0f5d4f9964
ntsakosurprise <mashelesepru@gmail.com>
2024-01-05 06:16:02 +0200

    chore(anzii): format code with prettier styles

--- packages/anzii/webpack.config.js
+++ packages/anzii/webpack.config.js
@@ -17,78 +17,78 @@ const root = path.resolve(__dirname);
 // console.log(root)
 
 const anzii = function () {
-  console.log("THE PROCESS MODE IS", process.env.NODE_MODE);
-  console.log("THE ENV IS", process.env.NODE_ENV);
-  console.log("OUR ROOT", root);
-  const isESM = process.env.NODE_MODE === "esm" ? true : false;
-  console.log("isESM", isESM);
+	console.log("THE PROCESS MODE IS", process.env.NODE_MODE);
+	console.log("THE ENV IS", process.env.NODE_ENV);
+	console.log("OUR ROOT", root);
+	const isESM = process.env.NODE_MODE === "esm" ? true : false;
+	console.log("isESM", isESM);
 
-  const config = {
-    entry: ["./lib/start"],
-    target: isESM ? false : "node",
-    // mode: 'development',
-    // devtool: 'source-map',
-    experiments: {
-      outputModule: isESM ? isESM : false,
-    },
-    cache: false,
-    // externals: [nodeExternals()],
-    externalsPresets: {
-      node: true, // in order to ignore built-in modules like path, fs, etc.
-    },
+	const config = {
+		entry: ["./lib/start"],
+		target: isESM ? false : "node",
+		// mode: 'development',
+		// devtool: 'source-map',
+		experiments: {
+			outputModule: isESM ? isESM : false,
+		},
+		cache: false,
+		// externals: [nodeExternals()],
+		externalsPresets: {
+			node: true, // in order to ignore built-in modules like path, fs, etc.
+		},
 
-    output: {
-      path: path.resolve("dist"),
-      filename: isESM ? "index.mjs" : "index.cjs",
-      libraryTarget: isESM ? "module" : "commonjs2",
-      chunkFormat: isESM ? "module" : "commonjs",
-      // clean: true
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          include: [root],
-          use: "babel-loader",
-        },
-      ],
-    },
+		output: {
+			path: path.resolve("dist"),
+			filename: isESM ? "index.mjs" : "index.cjs",
+			libraryTarget: isESM ? "module" : "commonjs2",
+			chunkFormat: isESM ? "module" : "commonjs",
+			// clean: true
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					include: [root],
+					use: "babel-loader",
+				},
+			],
+		},
 
-    plugins: [
-      new webpack.DefinePlugin({
-        __isBrowser__: "false",
-      }),
-      // new FileManagerPlugin({
-      //   onEnd: {
-      //       copy: [
-      //           { source:  path.resolve(__dirname, 'dist','index.cjs'), destination: path.resolve(__dirname, 'lib','index.cjs') },
-      //           // { source:  path.resolve(__dirname, 'dist','index.js.map'), destination: path.resolve(__dirname, 'lib','index.js.map') }
-      //           // { source: '/path/**/*.js', destination: '/path' },
-      //           // { source: '/path/fromfile.txt', destination: '/path/tofile.txt' },
-      //           // { source: '/path/**/*.{html,js}', destination: '/path/to' },
-      //           // { source: '/path/{file1,file2}.js', destination: '/path/to' },
-      //           // { source: '/path/file-[hash].js', destination: '/path/to' }
-      //       ]
-      //     }
-      //   })
-    ],
-    resolve: {
-      roots: [root],
-    },
-    // optimization: {
-    //   minimizer:[]
-    // minimizer: [new UglifyJsPlugin({
-    //   uglifyOptions:{
-    //     keep_classnames: false,
-    //     keep_fnames: false,
-    //     mangle: true
-    //   }
-    // })],
-    //},
-    // devtool: "source-map"
-  };
-  console.log("THE CONFIG", config);
-  return config;
+		plugins: [
+			new webpack.DefinePlugin({
+				__isBrowser__: "false",
+			}),
+			// new FileManagerPlugin({
+			//   onEnd: {
+			//       copy: [
+			//           { source:  path.resolve(__dirname, 'dist','index.cjs'), destination: path.resolve(__dirname, 'lib','index.cjs') },
+			//           // { source:  path.resolve(__dirname, 'dist','index.js.map'), destination: path.resolve(__dirname, 'lib','index.js.map') }
+			//           // { source: '/path/**/*.js', destination: '/path' },
+			//           // { source: '/path/fromfile.txt', destination: '/path/tofile.txt' },
+			//           // { source: '/path/**/*.{html,js}', destination: '/path/to' },
+			//           // { source: '/path/{file1,file2}.js', destination: '/path/to' },
+			//           // { source: '/path/file-[hash].js', destination: '/path/to' }
+			//       ]
+			//     }
+			//   })
+		],
+		resolve: {
+			roots: [root],
+		},
+		// optimization: {
+		//   minimizer:[]
+		// minimizer: [new UglifyJsPlugin({
+		//   uglifyOptions:{
+		//     keep_classnames: false,
+		//     keep_fnames: false,
+		//     mangle: true
+		//   }
+		// })],
+		//},
+		// devtool: "source-map"
+	};
+	console.log("THE CONFIG", config);
+	return config;
 };
 
 // var le = {
