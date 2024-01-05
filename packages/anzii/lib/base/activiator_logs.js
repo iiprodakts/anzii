7e0663ae67d6e7a4dd5a4fc27e4893c47cfc5095
ntsakosurprise <mashelesepru@gmail.com>
2024-01-05 09:47:11 +0200

    chore(anzii/modules): format

--- packages/anzii/lib/base/activiator_logs.js
+++ packages/anzii/lib/base/activiator_logs.js
@@ -1,7 +1,7 @@
 export const logInitializations = function (message = "") {
-    let initializationLogging = process.env.SHOW_ANZII_INITIALIZATIONS || 'false';
-    let shouldLog = initializationLogging && initializationLogging === 'true' ? true : false;
-    if (!shouldLog)
-        return;
-    console.log(`Initializing Anzii: ${message}`);
+	let initializationLogging = process.env.SHOW_ANZII_INITIALIZATIONS || "false";
+	let shouldLog =
+		initializationLogging && initializationLogging === "true" ? true : false;
+	if (!shouldLog) return;
+	console.log(`Initializing Anzii: ${message}`);
 };
