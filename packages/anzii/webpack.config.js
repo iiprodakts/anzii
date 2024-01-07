4d972c956a8793c7e4d0a53ff9e4d5343cc4cc6f
ntsakosurprise <mashelesepru@gmail.com>
2024-01-07 05:02:08 +0200

    chore(anzii): conditionally sets isESM

--- packages/anzii/webpack.config.js
+++ packages/anzii/webpack.config.js
@@ -7,22 +7,22 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 console.log("Webpack dir name", __dirname);
 console.log("webpack path", path.resolve(__dirname, "node_modules"));
-// const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-//const rootPath = path.resolve(__dirname);
-var front = {
+const isESM = process.env.NODE_MODE === "esm" ? true : false;
+
+const anzii = {
 	entry: "./lib/start.js",
 	target: "node13.14",
 	mode: "development",
+
 	experiments: {
-		outputModule: true,
+		outputModule: false,
 	},
 	//devtool: "inline-source-map",
 	output: {
-		path: path.join(__dirname, "dist"),
-		filename: "index.cjs",
-		library: {
-			type: "module",
-		},
+		path: path.resolve("dist"),
+		filename: isESM ? "index.mjs" : "index.cjs",
+		libraryTarget: isESM ? "module" : "commonjs2",
+		chunkFormat: isESM ? "module" : "commonjs",
 	},
 	externals: [
 		nodeExternals({
@@ -35,66 +35,19 @@ var front = {
 	],
 	resolve: {
 		extensions: [".js", ".jsx"],
-		// alias: {
-		//   Config: "/src/config/",
-		//   Components: "/src/components/",
-		//   HOC: "/src/hoc/",
-		//   Hooks: "/src/hooks/index",
-		//   Context: "/src/context/",
-		//   Utilities: "/src/utils/index",
-		//   Constants: "/src/constants/",
-		//   Assets: "/src/assets/",
-		//   AppGlobals: "/src/globals/index",
-		// },
 	},
 	module: {
 		rules: [
 			{
 				test: /\.(js|jsx)$/,
-				// exclude: /node_modules/,
 				exclude: [
 					path.resolve(__dirname, "node_modules"),
 					"/Users/surprisemashele/Documents/Development/frameworks/anzii/node_modules",
-
-					// exception: include these node_modules
-					// not: [
-					//   // add any node_modules that should be run through babel here
-					//   path.resolve(
-					//     rootPath,
-					//     "node_modules/@MY_ORG/MY_PACKAGE1"
-					//   ),
-					//   path.resolve(
-					//       rootPath,
-					//       "node_modules/@MY_ORG/MY_PACKAGE2"
-					//   ),
-					// ]
 				],
 				use: ["babel-loader"],
 			},
-			// {
-			//   test: /\.html$/,
-			//   use: "html-loader",
-			// },
-			/*Choose only one of the following two: if you're using 
-        plain CSS, use the first one, and if you're using a
-        preprocessor, in this case SASS, use the second one*/
-			//   {
-			//     test: /\.css$/,
-			//     use: ["style-loader", "css-loader"],
-			//   },
-			// {
-			//   test: /\.scss$/,
-			//   use: ["style-loader", "css-loader", "sass-loader"],
-			// },
 		],
+		exprContextCritical: false, // Temporary workaround
 	},
-
-	// plugins: [
-	//   new HTMLWebpackPlugin({
-	//     template: __dirname + "/public/index.html",
-	//     filename: "index.html",
-	//     inject: "body",
-	//   }),
-	// ],
 };
-export default front;
+export default anzii;
