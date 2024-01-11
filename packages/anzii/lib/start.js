8967f46d054d45d78f4d1343cbf08a6a9ea59fd3
ntsakosurprise <mashelesepru@gmail.com>
2024-01-11 21:02:31 +0200

    refactor(anzii): create named export for anzii
    In the latest source code of anzii, we have decided to write our code in
    es6 as a way to stay updated to the current recommended tools and to
    future-proof anzii.
    
    Nonetheless, as with most things in tech, not
    everyone or every system is in a ready state to migrate to
    the latest trends, as a result, we have also made a decision to offer
    support for users and systems that are still using commonjs module
    resulotions.
    
    In providing support for commonjs modules, we wanted to have both module
    resulotions use the same format to export and initialize anzii. This
    commit provides that functionality.

--- packages/anzii/lib/start.js
+++ packages/anzii/lib/start.js
@@ -1,18 +1,17 @@
-import anzii from "./anzii.js";
-const Anzii = anzii;
-export default function (modules = null) {
-    //  console.log('THE VALUE OF ACTIVATE')
-    //  console.log(Anzii)
-    // console.log(Components)
-    // console.log('The typeof Components')
-    // console.log(Components instanceof Array)
-    // console.log('The server Module')
-    // console.log(Server)
-    // Components.unshift({Server})
-    // let html = true
-    //    console.log('ANZII OBJECT')
-    //    console.log(Anzii)
-    //    console.log(Anzii.Activate)
-    modules ? Anzii.Activate([modules]) : Anzii.Activate();
-}
-
+import an from "./anzii.js";
+const Anzii = an;
+export const anzii = function (modules = null) {
+	//  console.log('THE VALUE OF ACTIVATE')
+	//  console.log(Anzii)
+	// console.log(Components)
+	// console.log('The typeof Components')
+	// console.log(Components instanceof Array)
+	// console.log('The server Module')
+	// console.log(Server)
+	// Components.unshift({Server})
+	// let html = true
+	//    console.log('ANZII OBJECT')
+	//    console.log(Anzii)
+	//    console.log(Anzii.Activate)
+	modules ? Anzii.Activate([modules]) : Anzii.Activate();
+};
