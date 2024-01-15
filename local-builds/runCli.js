22e92c21b70a7e5072a1461d4a430a2a0026ec9b
ntsakosurprise <mashelesepru@gmail.com>
2024-01-15 07:30:20 +0200

    chore(root): update local cli execution
    update cli execution for local package operations to cater for publish
    and compilation of the source doe.

--- local-builds/runCli.js
+++ local-builds/runCli.js
@@ -4,6 +4,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import createTarball from "./createTarball.js";
 import parseScriptArguments from "./parseScriptArguments.js";
+import publishYalcPackage from "./publishYalcPackage.js";
 import runNpmScript from "./runNpmScript.js";
 
 const dependecies = ["cjs-demo", "mjs-demo"];
@@ -18,62 +19,130 @@ console.log("PROCESS ARG", parseScriptArguments());
 //const workingDir = process.cwd();
 const possibleArgs = parseScriptArguments();
 if (possibleArgs.length === 0) {
-	throw new Error("RUNCLI requires the package name for tarball");
+	throw new Error("RUNCLI requires atleast one required script type to run");
 }
-const tarballPackageName = possibleArgs[0];
-const install = possibleArgs[1];
-const shouldInstallOnDeps = install && install === "install" ? true : false;
+const operationToRun = possibleArgs[0];
+
 const packagesPath = path.join(contextScriptRoot, "packages");
-const toBeTarballedPath = path.join(packagesPath, tarballPackageName);
-console.log("PackageToBeTarballed", toBeTarballedPath);
-const madeTarball = createTarball(toBeTarballedPath, tarballPackageName);
-if (shouldInstallOnDeps) {
-	chalk.green.bold(
-		console.info("A tarball package of anzii has successfully been created"),
-	);
-	chalk.green.bold(
-		console.info("System will install tarball package on dependecies next"),
-	);
-	dependecies.forEach(async (dep) => {
-		let packagePath = `${path.join(
-			process.cwd(),
-			`packages/${dep}/package.json`,
-		)}`;
-		let packageJSON = JSON.parse(
-			fs.readFileSync(packagePath, { encoding: "utf-8" }),
+
+if (operationToRun === "tarball") {
+	if (possibleArgs.length === 1) {
+		throw new Error("RUNCLI requires a package name to be tarballed");
+	}
+	const tarballPackageName = possibleArgs[1];
+	const install = possibleArgs[2];
+	const shouldInstallOnDeps = install && install === "install" ? true : false;
+	const toBeTarballedPath = path.join(packagesPath, tarballPackageName);
+	const madeTarball = createTarball(toBeTarballedPath, tarballPackageName);
+	if (shouldInstallOnDeps) {
+		chalk.green.bold(
+			console.info("A tarball package of anzii has successfully been created"),
 		);
-
-		let packageJSONScripts = packageJSON.scripts;
-
-		packageJSONScripts = {
-			...packageJSONScripts,
-			installTarball: `npm i file:../anzii/${madeTarball}`,
-		};
-
-		packageJSON = { ...packageJSON, scripts: { ...packageJSONScripts } };
-		fs.writeFileSync(`${packagePath}`, JSON.stringify(packageJSON, null, 2));
-		let runResult = await runNpmScript(
-			"run",
-			`--prefix ${path.join(process.cwd(), `packages/${dep}`)} installTarball`,
-			"",
-			process.cwd(),
+		chalk.green.bold(
+			console.info("System will install tarball package on dependecies next"),
 		);
-		if (runResult) {
-			chalk.cyanBright.bold(
-				console.info(
-					`Tarball package has successfully been installed on package ${dep}`,
-				),
+		dependecies.forEach(async (dep) => {
+			let packagePath = `${path.join(
+				process.cwd(),
+				`packages/${dep}/package.json`,
+			)}`;
+			let packageJSON = JSON.parse(
+				fs.readFileSync(packagePath, { encoding: "utf-8" }),
 			);
-		} else {
-			chalk.redBright.bold(
-				console.info(
-					"Tarball package could not be installed. An error occured:",
-					runResult,
-				),
+
+			let packageJSONScripts = packageJSON.scripts;
+
+			packageJSONScripts = {
+				...packageJSONScripts,
+				installTarball: `npm i file:../anzii/${madeTarball}`,
+			};
+
+			packageJSON = { ...packageJSON, scripts: { ...packageJSONScripts } };
+			fs.writeFileSync(`${packagePath}`, JSON.stringify(packageJSON, null, 2));
+			let runResult = await runNpmScript(
+				"run",
+				`--prefix ${path.join(
+					process.cwd(),
+					`packages/${dep}`,
+				)} installTarball`,
+				"",
+				process.cwd(),
 			);
+			if (runResult) {
+				chalk.cyanBright.bold(
+					console.info(
+						`Tarball package has successfully been installed on package ${dep}`,
+					),
+				);
+			} else {
+				chalk.redBright.bold(
+					console.info(
+						"Tarball package could not be installed. An error occured:",
+						runResult,
+					),
+				);
+			}
+		});
+		console.log("madeTARBALL", madeTarball);
+	}
+} else if (operationToRun === "publish") {
+	publishYalcPackage("anzii")
+		.then((publishResult) => {
+			console.log("PUSH RESULT", publishResult);
+			dependecies.forEach(async (dep) => {
+				let runResult = await runNpmScript(
+					"run",
+					`--prefix ${path.join(process.cwd(), `packages/${dep}`)} yalc:add`,
+					"",
+					process.cwd(),
+				);
+				if (runResult) {
+					chalk.cyanBright.bold(
+						console.info(
+							`Yalc added package has successfully been installed on package ${dep}`,
+						),
+					);
+				} else {
+					chalk.redBright.bold(
+						console.info(
+							"Yalc add package could not be installed. An error occured:",
+							runResult,
+						),
+					);
+				}
+			});
+		})
+		.catch((err) => {
+			console.log(
+				"An error occured trying to perform publish/add operations",
+				err,
+			);
+		});
+} else if (operationToRun === "copubin") {
+	runNpmScript(
+		"run",
+		`--prefix ${path.join(process.cwd(), `packages/anzii`)} compile`,
+		"",
+		process.cwd(),
+	).then((result) => {
+		if (result) {
+			runNpmScript(
+				"run",
+				`--prefix ${process.cwd()} local:publish`,
+				"",
+				process.cwd(),
+			).then((localPublishRes) => {
+				if (localPublishRes) {
+					console.log(
+						"Anzii Package has succesfully been published and updated",
+					);
+					console.log(
+						"Anzii dependent packages have been updated with the latest version",
+					);
+				}
+			});
 		}
 	});
-	console.log("madeTARBALL", madeTarball);
 }
 
 // const nodeScriptPath = path.join(packagesPath, "kotii-cli");
