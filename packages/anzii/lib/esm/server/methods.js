09081476adb5e8954ee36cb5e7aafdd87e58ba60
ntsakosurprise <mashelesepru@gmail.com>
2025-10-06 02:07:53 +0200

    chore(packages/anzii): refactor request plugin with better naming

--- packages/anzii/lib/esm/server/methods.js
+++ packages/anzii/lib/esm/server/methods.js
@@ -53,26 +53,27 @@ export const runServer = function (data) {
 	});
 };
 
-export const handleWriteServerRequestResponse = async function (data) {
+export const handleWriteServerRequestResponse = async function (response) {
 	const self = this;
 
-	if (data.method === "stream") {
-		return self.streamResponse(data);
-	} else if (data.method === "renderView") {
-		let view = data.data.data;
-		data.res.set("Connection", "close");
+	const { method, payload, res, code = 200 } = response;
+	const { view = null, toCLientPayload = null } = payload;
+	if (method === "stream") {
+		return self.streamResponse(response);
+	} else if (method === "renderView") {
+		res.set("Connection", "close");
 
 		if (view.type.trim() === "template") {
 			self
-				.getHtml(data.res, view)
+				.getHtml(res, view)
 				.then(async (html) => {
 					if (html.success) {
-						data.res.status(200).send(html.html),
+						res.status(200).send(html.html),
 							// eslint-disable-next-line no-mixed-spaces-and-tabs
 							// eslint-disable-next-line no-mixed-spaces-and-tabs
 							self.infoSync(
 								`SERVER HAS SUCCESSFULLY SENT RESPONSE TO CLIENT WITH RESPONSE ID::${
-									data.res.R_ID.split("-")[0]
+									res.R_ID.split("-")[0]
 								}`,
 								// eslint-disable-next-line no-mixed-spaces-and-tabs
 							);
@@ -86,85 +87,87 @@ export const handleWriteServerRequestResponse = async function (data) {
 					}
 				})
 				.catch((e) => {
-					data.res.status(500).send(e.html);
+					res.status(500).send(e.html);
 				});
 			return;
 		} else if (view.type === "modular") {
-			data.res.status(200).send(view.view);
+			res.status(200).send(view.view);
 			return self.infoSync(
 				`SERVER HAS SUCCESSFULLY SENT RESPONSE TO CLIENT WITH RESPONSE ID::${
-					data.res.R_ID.split("-")[0]
+					res.R_ID.split("-")[0]
 				}`,
 			);
 		}
 	} else {
-		data.R_ID
+		res.R_ID
 			? self.infoSync(
 					`SERVER IS ABOUT TO SEND RESPONSE BACK TO CLIENT WITH RESPONSE ID::${
-						data.res.R_ID.split("-")[0]
+						res.R_ID.split("-")[0]
 					}`,
 			  )
 			: self.infoSync(
 					`SERVER IS ABOUT TO SEND RESPONSE BACK TO CLIENT WITH RESPONSE`,
 			  );
 
-		await data.res.set("Connection", "close");
-		if (data.data.accepts) {
-			switch (data.data.accepts) {
-				case "json":
-					data.res.status(200).send(data.data);
-					break;
-				case "html":
-					self
-						.getHtml(data.res, {
-							view: "main/404",
-							title: "Page could not be found",
-						})
-						.then((html) => {
-							return data.res.status(400).send(html.html);
-						})
-						.catch((e) => {
-							return data.res.status(500).send(e.html);
-						});
-					break;
-				default:
-					data.res.type("txt").status(400).send("Text not found");
-			}
-		} else {
-			data.res.status(200).send(data.data);
+		await res.set("Connection", "close");
+
+		switch (res.ACCEPTS) {
+			case "json":
+				res.status(code).send(toCLientPayload);
+				break;
+			case "html":
+				self
+					.getHtml(res, {
+						view: "main/404",
+						title: "Page could not be found",
+					})
+					.then((html) => {
+						return res.status(400).send(html.html);
+					})
+					.catch((e) => {
+						return res.status(500).send(e.html);
+					});
+				break;
+			case "txt":
+				res.type("txt").status(404).send("Text not found");
+				break;
+			default:
+				res.status(406).send("Not acceptable");
 		}
+
 		// await data.res.end()
 		// // self.debug(data.data)
 		return await self.log("SERVER HAS SENT A RESPONSE BACK TO CLIENT");
 	}
 };
-export const streamResponse = function (data) {
+export const streamResponse = function (response) {
 	const self = this;
 	const pao = self.pao;
-	const type = self.mimeTypes[data.data.ext];
-	let rStream = data.data.rStream;
-	const withAttachment = data.data.withAttachment || null;
-	if (withAttachment) data.res["withAttachment"] = { ...withAttachment };
+	const { payload, res } = response;
+	const { ext, rStream = "", withAttachment = null } = payload;
+	const type = self.mimeTypes[ext];
+
+	if (withAttachment) res["withAttachment"] = { ...withAttachment };
 
 	rStream.on("open", async function () {
-		data.res.set("Content-Type", type);
-		data.res.set("Connection", "close");
-		return rStream.pipe(data.res);
+		res.set("Content-Type", type);
+		res.set("Connection", "close");
+		return rStream.pipe(res);
 		// return await self.log('SERVER HAS SENT A STREAM RESPONSE BACK TO THE CLIENT ENDING REQUEST SOON')
 	});
 	rStream.on("end", async function () {
-		data.res.end();
+		res.end();
 		// return await self.log('Stream request has been successfully served WITH END')
 		return self.infoSync(
 			`STREAM REQUEST HAS BEEN SUCCESSFULLY HANDLED WITH RESPONSE ID::${
-				data.res.R_ID.split("-")[0]
+				res.R_ID.split("-")[0]
 			}`,
 		);
 	});
 	rStream.on("error", async function (e) {
-		data.res.set("Content-Type", "application/json");
-		data.res.set("Connection", "close");
-		return data.res.status(404).send({ error: true, message: "Not found" });
+		res.set("Content-Type", "application/json");
+		res.set("Connection", "close");
+		return res.status(404).send({ error: true, message: "Not found" });
 
 		// return  await self.log('SERVER HAS SENT A STREAM RESPONSE BACK TO THE CLIENT WITH ERROR')
 	});
